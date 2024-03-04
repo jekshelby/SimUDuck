0132d47b9d24de2a5f1c694d23dad0bea792693a
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-03-04 21:22:20 +0700

    versi 3 add rubber duck and override some methods

---
+++ SimUDuck/src/SimUDuckV3/RedheadDuck.java
@@ -0,0 +1,11 @@
+package SimUDuckV3;
+
+public class RedheadDuck extends Duck {
+    @Override
+    public void display() {
+        System.out.println("Redhead Duck");
+        System.out.println(quack());
+        System.out.println(swim());
+        System.out.println(fly());
+    }
+}
---
+++ SimUDuck/src/SimUDuckV3/RubberDuck.java
@@ -0,0 +1,20 @@
+package SimUDuckV3;
+
+public class RubberDuck extends Duck {
+    @Override
+    public String quack() {
+        return "Berdecit(tidak quack)";
+    }
+
+    @Override
+    public String fly() {
+        return "Tidak Bisa Terbang";
+    }
+    @Override
+    public void display() {
+        System.out.println("Rubber Duck");
+        System.out.println(quack());
+        System.out.println(swim());
+        System.out.println(fly());
+    }
+}
